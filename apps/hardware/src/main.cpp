6a2d0685c1a92f10116a197163a6b682959396f7
ptrn23 <pnecasio23@gmail.com>
2026-04-15 21:41:25 +0800

    feat: setup hardware

---
+++ apps/hardware/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("\n--- IPhilSafe Hardware Edge Initialized ---");
+  
+  // TODO
+}
+
+void loop() {
+  // TODO
+}
