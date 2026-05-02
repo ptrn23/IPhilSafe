4afcbb171fa21c21a432011b04b22650cf4c83ba
silinaspicy <cellinlouise.cheng@gmail.com>
2026-05-02 18:26:37 +0800

    feat: api/get-audit-logs a GET api call to get all audit logs for admin users only

---
+++ apps/web/app/api/get-audit-logs/[user_id]/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from "@repo/db";
+import { get_locker_state } from "../../utils";
+
+export async function GET(
+    req: Request,
+    { params }: { params: { user_id: string } }
+) {
+  try {
+    const {user_id}  = await params;
+    const u_id = parseInt(user_id, 10)
+
+    console.log("Audit logs accessed by user:", u_id);
+
+    const user = await prisma.user.findFirst({
+      where: { uinPhilsys: u_id }
+    });
+
+    // 2. Strict check for the ID
+    if (user === undefined || user === null) {
+      return NextResponse.json({ error: "No user in the database" }, { status: 400 });
+    }
+
+    // 2. Strict check for the ID
+    if (user.userRole != 'Admin') {
+      return NextResponse.json({ error: "User role is not admin" }, { status: 400 });
+    }
+                      
+    const res = await prisma.auditLog.findMany();
+    const safeRes = JSON.parse(
+    JSON.stringify(res, (_, value) =>
+        typeof value === "bigint" ? value.toString() : value
+      )
+    );
+
+    return NextResponse.json(safeRes);     
+  } catch (err) {
+    console.error("Status Route Error:", err);
+    return NextResponse.json(
+      { error: "Internal Server Error", details: err instanceof Error ? err.message : String(err) },
+      { status: 500 }
+    );
+  }
+}
