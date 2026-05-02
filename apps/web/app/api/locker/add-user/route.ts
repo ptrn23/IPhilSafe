b00ea4e0a43b043129cd3b96c9bef087e4631747
silinaspicy <cellinlouise.cheng@gmail.com>
2026-05-02 18:28:29 +0800

    feat: api/locker-add-user a POST api call to add a user to a locker only during the registrations period

---
+++ apps/web/app/api/locker/add-user/route.ts
@@ -0,0 +1,78 @@
+import { NextResponse } from 'next/server';
+import { prisma } from '@repo/db';
+import { create_audit_log } from '../../utils';
+export async function POST(
+    req: NextResponse
+) {
+  try {
+    const { qrData, locker_id } = await req.json();
+
+    const user_data = JSON.parse(qrData);
+    const uin = user_data.subject.uin
+    const first_name = user_data.subject.fname
+    const l_id = parseInt(locker_id, 10)
+
+    const locker = await prisma.locker.findUnique({
+      where: { lockerId: l_id }
+    });
+    let user = await prisma.user.findUnique({
+      where: { uinPhilsys: uin }
+    });
+
+    // check if locker exists
+    if (!locker){
+      return NextResponse.json({ error: `Locker ${l_id} not found` }, { status: 404 });
+    }
+
+    // create user if it does not exist
+    if (!user) {
+      user = await prisma.user.create({
+        data: { 
+          uinPhilsys: uin, 
+          firstName: first_name, 
+          userRole: "User" }
+      });
+    }
+
+    // check if in registering period
+    const latestRegLog = await prisma.auditLog.findFirst({
+      where: {
+        lockerId: l_id, 
+        sysType: "Registering"
+      },
+      orderBy: {createdAt: "desc"},
+    })
+
+    const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000);
+
+    if (!(latestRegLog?.createdAt && latestRegLog.createdAt > fiveMinutesAgo)){
+      return NextResponse.json({ error: `Past Registration period or registration hasn't started` }, { status: 404 });
+    }
+
+    // check for duplicates already added
+    const isAdded = await prisma.userLocker.findFirst({
+      where: {
+        lockerId: l_id,
+        userId: uin
+      }
+    })
+    if (isAdded){
+      return NextResponse.json({ error: `User ${first_name} with uin ${uin} is already a user for locker ${l_id}` }, { status: 404 });
+    }
+
+    // add user to locker
+    await prisma.userLocker.create({
+      data: { 
+        userId: uin, 
+        lockerId: l_id }
+    });
+
+    // create log
+    create_audit_log(l_id, 'Added_user', "New user added", uin);
+    
+    return NextResponse.json({ message: `user, ${first_name} with uin ${uin}, has been added as user to locker ${l_id}`});
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
