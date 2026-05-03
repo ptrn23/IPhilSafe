0473edd34c60d835084d608c69b078ae43a012ed
Azul Eusebio <eusebio.ado@gmail.com>
2026-05-03 18:25:15 +0800

    (api) refactor: simplified open-locker API call format, updated expected JSON payload, integrated MOSIP verification, updated sys_log_type in log creation

--- apps/web/app/api/locker/open-locker/route.ts
+++ apps/web/app/api/locker/open-locker/route.ts
@@ -1,20 +1,25 @@
 import { NextResponse, NextRequest } from 'next/server';
 import { prisma } from '@repo/db';
-import { create_audit_log } from '../../../../utils';
+import { create_audit_log, verifyWithMOSIP } from '../../utils';
 export async function POST(
     req: NextRequest,
-    { params }: { params: Promise<{ user_id:string, locker_id: string }> }
-
 ) {
   try {
-    const { user_id, locker_id } = await params;
+    const { qrData, locker_id } = await req.json();
     // Strict check for the params
-    if (!user_id || !locker_id) {
+    if (!qrData || !locker_id) {
       return NextResponse.json({ error: "Route parameters not found" }, { status: 400 });
     }
+    const user_data = JSON.parse(qrData);
+    const uin = user_data.subject.uin;
+    const name = user_data.subject.name;
+    const l_id = Number(locker_id);
 
-    const uin = parseInt(user_id, 10)
-    const l_id = parseInt(locker_id, 10)
+    // MOSIP verification
+    const mosipResult = await verifyWithMOSIP(JSON.stringify(user_data.subject));
+    if (mosipResult.status !== "verified") {
+      return NextResponse.json({ error: `MOSIP verification failed: ${mosipResult.message || "Unknown error"}` }, { status: 401 });
+    }
 
     // check if user and locker exists
     const user = await prisma.user.findUnique({
@@ -38,15 +43,16 @@ export async function POST(
       }
     });
 
+    // create log and return response based on verification result
     if (isUserVerified){
       console.log("authorized locker")
-      create_audit_log(l_id, 'Locker_Accesed', "Locker opened by user", uin )      
-      return NextResponse.json({ message: "Authorized", name: user.firstName });
+      create_audit_log(l_id, 'Locker_Opened', "Locker opened by user", uin )      
+      return NextResponse.json({ message: "Authorized", name: user.name });
     }
     else{
-      console.log("desnied locker")
-      create_audit_log(l_id, 'Locker_Accesed', "Locker denied access to user", uin )      
-      return NextResponse.json({ message: "Denied", name: user.firstName });
+      console.log("denied locker")
+      create_audit_log(l_id, 'Denied_Access', "Locker denied access to user", uin )      
+      return NextResponse.json({ message: "Denied", name: user.name });
     }
   } catch (e) {
     console.error(e);
